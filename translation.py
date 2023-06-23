8149fdf881f281bb955c465a9e4597934f0a5821
mknapp62 <96498324+mknapp62@users.noreply.github.com>
2023-06-23 11:42:33 -0700

    Update translation.py
    Updated for meta's api in the elastic beanstalk aws

--- translation.py
+++ translation.py
@@ -1,28 +1,24 @@
-###THIS IS THE FLASK API FOR THE TRANSLATION.JS APPROACH
-from flask import Flask, request, send_file, jsonify #, url_for, redirect, jsonify
+###THIS IS THE LIVE FILE
+from flask import Flask, request
 from flask_cors import CORS
-#from flask_restful import Api, Resource
-import os
 import requests
-import csv
-import keras_nlp
-import tensorflow as tf
-from tensorflow import keras
-app = Flask(__name__)
-cors = CORS(app)
-@app.route('/translator', methods=['POST'])
+application = Flask(__name__)
+application.config['PREFERRED_URL_SCHEME'] = 'https'
+CORS(application)
+@application.route('/', methods=['POST'])
 def load_and_translate():
-    # Send request to Deepgram API
-    filename = request.json['text_input']
-    url = "https://api.deepgram.com/v1/listen?punctuate=true"
+    data = request.json.get('text_input','lang_num')
+    text_input = data[0]
+    lang_num = int(data[1])
+
+    lang = ["RO", "ZH", "EN-US", "EN"]
 
     # Set up DeepL API request
     url = 'https://api-free.deepl.com/v2/translate'
     params = {
         "auth_key": "954afa95-a70d-41f0-c9c2-9d29d644acf6:fx",
-        "text": filename,
-        "source_lang": "en",
-        "target_lang": "es",
+        "text": text_input,
+        "target_lang": lang[lang_num],
     }
 
     # Send request to DeepL API
@@ -30,5 +26,5 @@ def load_and_translate():
 
     # Get translated text from DeepL API response
     translated_text = response.json()["translations"][0]["text"]
-    #text_to_speech(translated_text) THIS WILL BE THE WEBSOCKET TO SEND LIVE TEXT TO LISTENER'S WEBPAGE
     return translated_text
+
