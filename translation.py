baa239b632de94a7264ba131281ba24c6bd2b179
mknapp62 <96498324+mknapp62@users.noreply.github.com>
2023-08-17 12:15:07 -0700

    Update translation.py
    added initial phase two functionality

--- translation.py
+++ translation.py
@@ -1,7 +1,10 @@
 ###THIS IS THE LIVE FILE
-from flask import Flask, request
+from flask import Flask, render_template, request, send_file
+import os
 from flask_cors import CORS
 import requests
+import boto3
+
 application = Flask(__name__)
 application.config['PREFERRED_URL_SCHEME'] = 'https'
 CORS(application)
@@ -27,4 +30,32 @@ def load_and_translate():
     # Get translated text from DeepL API response
     translated_text = response.json()["translations"][0]["text"]
     return translated_text
+
+
+
+@application.route('/MetaFrazoDubbedTranslation')
+def hello():
+    """Renders a sample page."""
+    return render_template('dubbedpage.html')
+
+@application.route('/upload', methods=['POST'])
+def upload_video():
+    # AWS S3 configuration
+    s3 = boto3.client('s3')
+    S3_BUCKET = 'elasticbeanstalk-us-east-2-039300220315'
+
+
+    file = request.files['videoFile']
+    # Save the uploaded video file to a temporary directory
+    if file.filename == '':
+        return "No selected file.", 400
+
+    # Generate a valid filename
+    filename = 'test.mp4'
+
+    # Upload the file to S3
+    s3.upload_fileobj(file, S3_BUCKET, filename)
+
+    # Provide a response to the client
+    return "File uploaded successfully."
 
